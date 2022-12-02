7a6bd3212c853bd9d2df29d61ab217b70bbc30b7
Harkiran7237 <60743030+Harkiran7237@users.noreply.github.com>
2022-12-02 06:20:00 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,62 +1,57 @@
 import socket
 from threading import Thread
-from tkinter import *
 
-# nickname = input("Choose your nickname: ")
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 ip_address = '127.0.0.1'
 port = 8000
 
-client.connect((ip_address, port))
+server.bind((ip_address, port))
+server.listen()
 
-print("Connected with the server...")
+list_of_clients = []
+nicknames = []
 
-class GUI:
-    def __init__(self):
-        self.Window = Tk()
-        self.Window.withdraw()
+print("Server has started...")
 
-        self.login = Toplevel()
-        self.login.title("Login")
+def clientthread(conn, nickname):
+    conn.send("Welcome to this chatroom!".encode('utf-8'))
+    while True:
+        try:
+            message = conn.recv(2048).decode('utf-8')
+            if message:
+                print(message)
+                broadcast(message, conn)
+            else:
+                remove(conn)
+                remove_nickname(nickname)
+        except:
+            continue
 
-        self.login.resizable(width=False, height=False)
-        self.login.configure(width=400, height=300)
-        
-        self.pls = Label(self.login,text = "Please login to continue",justify = CENTER,font = "Helvetica 14 bold")
-        self.pls.place( relheight = 0.15,relx = 0.2,rely = 0.07)
+def broadcast(message, connection):
+    for clients in list_of_clients:
+        if clients!=connection:
+            try:
+                clients.send(message.encode('utf-8'))
+            except:
+                remove(clients)
 
-        self.labelName = Label(self.login,text = "Name: ",font = ("Helvetica 12"))
-		#self.labelName.place(relheight=0.2,)
-        self.labelName.place(relheight = 0.2,relx = 0.1,rely = 0.2)
+def remove(connection):
+    if connection in list_of_clients:
+        list_of_clients.remove(connection)
 
-        self.entryName = Entry(self.login,font = "Helvetica 14")
-		# self.entryName.place(relwidth = 0.4,relheight = 0.12,relx = 0.35,rely = 0.2)
-        # self.entryName.focus()
+def remove_nickname(nickname):
+    if nickname in nicknames:
+        nicknames.remove(nickname)
 
-g = GUI()
-
-
-# def receive():
-#     while True:
-#         try:
-#             message = client.recv(2048).decode('utf-8')
-#             if message == 'NICKNAME':
-#                 client.send(nickname.encode('utf-8'))
-#             else:
-#                 print(message)
-#         except:
-#             print("An error occured!")
-#             client.close()
-#             break
-
-# def write():
-#     while True:
-#         message = '{}: {}'.format(nickname, input(''))
-#         client.send(message.encode('utf-8'))
-
-# receive_thread = Thread(target=receive)
-# receive_thread.start()
-# write_thread = Thread(target=write)
-# write_thread.start()
+while True:
+    conn, addr = server.accept()
+    conn.send('NICKNAME'.encode('utf-8'))
+    nickname = conn.recv(2048).decode('utf-8')
+    list_of_clients.append(conn)
+    nicknames.append(nickname)
+    message = "{} joined!".format(nickname)
+    print(message)
+    broadcast(message, conn)
+    new_thread = Thread(target= clientthread,args=(conn, nickname))
+    new_thread.start()
